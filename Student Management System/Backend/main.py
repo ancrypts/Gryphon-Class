a7efe173ccfaf4cea7f5ce16383471f430e30c64
Animesh Kumar <218124176+ancrypts@users.noreply.github.com>
2026-08-09 14:39:41 +0530

    Update main.py

--- Student Management System/Backend/main.py
+++ Student Management System/Backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],      # GET, POST, PUT, DELETE, etc.
     allow_headers=["*"],      # Allow all headers
 )
